531a115804020768e179158d3d540357c6ad7eb8
RedFu01 <konradfuger@googlemail.com>
2018-07-05 09:39:13 +0200

    implement full data for urban

--- loadFullAircraftDataHttp.m
+++ loadFullAircraftDataHttp.m
@@ -2,17 +2,22 @@ function [ aircraft ] = loadFullAircraftDataHttp( scenario, timestamp, varargin 
 %LOADAIRCRAFTHTTP Summary of this function goes here
 %   Detailed explanation goes here
 
-    if(scenario ~= 'rural')
-        error('At the moment only rural scenario is supported');
-    end
+    % if(scenario ~= 'rural')
+    %     error('At the moment only rural scenario is supported');
+    %end
     try
+        if(scenario == 'rural')
+            db = 'skyhigh-spotbeam-pos_rural_150';
+        else
+            db = 'skyhigh-spotbeam-pos_urban_150';
+        end
         if(length(varargin) > 0)
             port = varargin(1);
             
-            url = strcat('http://localhost:', port, '/spotbeam/full?timestamp=',num2str(timestamp), '&db=skyhigh-spotbeam-pos_rural_75');
+            url = strcat('http://localhost:', port, '/spotbeam/full?timestamp=',num2str(timestamp), '&db=', db);
             url = strjoin(url, '');
         else
-            url = strcat('http://orion6:3000/spotbeam/full?timestamp=',num2str(timestamp), '&db=skyhigh-spotbeam-pos_rural_75');
+            url = strcat('http://orion6:3000/spotbeam/full?timestamp=',num2str(timestamp), '&db=', db);
         end
         aircraft = webread(url);
    catch
